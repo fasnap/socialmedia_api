e05fa2b39afcf017743e755fc344b501e94bd70c
fasnap <fasnapkdy@gmail.com>
2021-12-15 18:29:48 +0530

    deploy error

--- social_media/settings.py
+++ social_media/settings.py
@@ -127,4 +127,12 @@ REST_FRAMEWORK = {
     )
 }
 
+from datetime import timedelta
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
+
 django_heroku.settings(locals())
